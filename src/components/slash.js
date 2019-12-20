0450a61afb2b4805d262fbb53ed8bf54fba6b3d6
viktorrenkema <vrenkema@gmail.com>
2019-12-20 23:32:26 +0100

    Fix slash

--- src/components/slash.js
+++ src/components/slash.js
@@ -2,40 +2,15 @@ import * as React from "react"
 import { motion } from "framer-motion"
 
 export default function Slash(props) {
-  // const slash = {
-  //   normal: {
-  //     rotate: 180,
-  //   },
-  //   hovered: {
-  //     rotate: 360,
-  //   },
-  // }
-
-  // const [hovered, setHovered] = React.useState(false)
-
-  // const onHoverStart = () => {
-  //   setHovered(true)
-  // }
-
-  // const onHoverEnd = () => {
-  //   setHovered(false)
-  // }
-
   return (
-    <motion.div
-      className="slash"
-      // onHoverStart={onHoverStart}
-      // onHoverEnd={onHoverEnd}
-    >
-      <motion.svg xmlns="http://www.w3.org/2000/svg" width="36.5" height="65.5">
+    <motion.div className="slash">
+      <motion.svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="36.5"
+        height="65.5"
+        overflow="visible"
+      >
         <motion.path
-          // variants={slash}
-          // initial="normal"
-          // animate={hovered ? "hovered" : "normal"}
-          // transition={{
-          //   default: { duration: 2, ease: "easeInOut" },
-          //   fill: { duration: 2, ease: [1, 0, 0.8, 1] },
-          // }}
           d="M 0 65.5 L 36.5 0"
           strokeWidth="4"
           strokeLinecap="round"
