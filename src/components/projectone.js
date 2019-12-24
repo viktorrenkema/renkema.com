31d074980d7ce6f1edb7603b773f69dddb85e508
viktorrenkema <vrenkema@gmail.com>
2019-12-24 13:17:38 +0100

    Fix projectcopy being pushed to bottom

--- src/components/projectone.js
+++ src/components/projectone.js
@@ -3,26 +3,28 @@ import { motion } from "framer-motion"
 import accordion from "../../src/images/accordion.gif"
 
 export default function ProjectOne(props) {
-  const cardHover = {
-    inactive: {
-      width: "300px",
-      transition: { duration: 0.6 },
-    },
-    active: {
-      width: "2500px",
-      transition: { duration: 0.4 },
-    },
-  }
+  // const cardHover = {
+  //   inactive: {
+  //     width: "300px",
+  //     transition: { duration: 0.6 },
+  //   },
+  //   active: {
+  //     width: "2500px",
+  //     transition: { duration: 0.4 },
+  //   },
+  // }
 
   const overlay = {
     hidden: {
+      display: "none",
       opacity: 0,
-      // x: -100,
+      width: "0px",
     },
     visible: {
+      display: "flex",
       opacity: 1,
-      // x: 0,
-      transition: { delay: 0.4 },
+      width: "900px",
+      transition: { duration: 0.4 },
     },
   }
 
@@ -38,14 +40,9 @@ export default function ProjectOne(props) {
 
   return (
     <motion.div
-      variants={cardHover}
-      style={{ overflow: "hidden" }}
-      className="card_projectone"
       onHoverStart={onHoverStartImage}
       onHoverEnd={onHoverEndImage}
-      initial="inactive"
-      animate={hoveredImage ? "active" : "inactive"}
-      transition={{ duration: 0.5 }}
+      className="card_projectone"
     >
       <motion.div className="wrap_imagecardone">
         <a
@@ -67,22 +64,25 @@ export default function ProjectOne(props) {
         variants={overlay}
         animate={hoveredImage ? "visible" : "hidden"}
         initial="hidden"
-        transition={{ duration: 0.1 }}
+        transition={{ duration: 0.4 }}
       >
         <p className="copy_projecttitle">Creating an accordion design</p>
         <p class="copy_projectlabel">FRAMER LEARN TUTORIAL</p>
-        <p className="projectdescriptions">
-          While learning React, I enjoyed documenting projects that I worked on.
-          One of these turned into a 3-part tutorial, which guides users through
-          creating a single-select accordion effect within Framer. <br></br>
-          <br></br>It touches upon various concepts, such as:
-          <br></br>
-          <ul>
+        <div style={{ display: "flex" }}>
+          <p className="projectdescriptions">
+            While learning React, I enjoyed documenting projects that I worked
+            on. One of these turned into a 3-part tutorial, which guides users
+            through creating a single-select accordion effect within Framer.{" "}
+            <br></br>
+            <br></br>
+          </p>
+          <ul className="projectdescriptions">
+            <p>It touches upon various concepts, such as:</p>
             <li>applying React Hooks to toggle states</li>
             <li>creating custom animations</li>
             <li>mapping over an array with JSON data</li>
           </ul>
-        </p>
+        </div>
       </motion.div>
     </motion.div>
   )
