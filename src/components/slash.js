2ec8b11f901bb0049d7069a2ebf6e3b25b34c7de
viktorrenkema <vrenkema@gmail.com>
2019-12-26 17:59:05 +0100

    Redesign slash

--- src/components/slash.js
+++ src/components/slash.js
@@ -6,15 +6,16 @@ export default function Slash(props) {
     <motion.div className="slash">
       <motion.svg
         xmlns="http://www.w3.org/2000/svg"
-        width="36.5"
-        height="65.5"
+        width="46"
+        height="82"
         overflow="visible"
       >
         <motion.path
-          d="M 0 65.5 L 36.5 0"
-          strokeWidth="4"
-          strokeLinecap="round"
+          d="M 43 6 L 5 73.5 L 2 72 L 40 4.5 Z"
+          strokeWidth="1"
           stroke="#ff6661"
+          // stroke="rgb(255,102,97)"
+          fill="transparent"
         ></motion.path>
       </motion.svg>
     </motion.div>
