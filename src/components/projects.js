4e18baa4db0d6f812c180a4f4c5a5a75de769c2b
viktorrenkema <vrenkema@gmail.com>
2020-01-12 22:11:34 +0100

    Add stagger from bottom to project cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -28,19 +28,28 @@ export default function Projects(props) {
   }
 
   const header = {
-    hidden: { y: -20, opacity: 0, transition: { duration: 1.3 } },
+    hidden: { y: -20, opacity: 0, transition: { duration: 0.5 } },
+    visible: { y: 0, opacity: 1, transition: { duration: 0.5 } },
+  }
+
+  const card = {
+    hidden: { y: 20, opacity: 0, transition: { duration: 0.5 } },
     visible: { y: 0, opacity: 1, transition: { duration: 0.5 } },
   }
 
   const [isInViewport, targetRef] = useIsInViewport({ threshold: 10 })
+  const [FirstCardInViewport, targetRef1] = useIsInViewport({ threshold: 40 })
+  const [SecondCardInViewport, targetRef2] = useIsInViewport({ threshold: 40 })
+  const [ThirdCardInViewport, targetRef3] = useIsInViewport({ threshold: 40 })
+  const [FourthCardInViewport, targetRef4] = useIsInViewport({ threshold: 40 })
 
   return (
     <motion.section
+      initial="hidden"
+      animate={isInViewport ? "visible" : "hidden"}
       className="projects"
-      variants={container}
-      initial="visible"
       ref={targetRef}
-      // animate={isInViewport ? "visible" : "hidden"}
+      variants={container}
     >
       <motion.h1
         className="h1-projects header"
@@ -50,19 +59,43 @@ export default function Projects(props) {
       >
         Projects
       </motion.h1>
-      <motion.div className="section_project">
+      <motion.div
+        className="section_project"
+        ref={targetRef1}
+        animate={FirstCardInViewport ? "visible" : "hidden"}
+        variants={card}
+        initial="hidden"
+      >
         <ProjectOne variants={item}></ProjectOne>
       </motion.div>
 
-      <motion.div className="section_project">
+      <motion.div
+        className="section_project"
+        ref={targetRef2}
+        animate={SecondCardInViewport ? "visible" : "hidden"}
+        variants={card}
+        initial="hidden"
+      >
         <ProjectTwo variants={item}></ProjectTwo>
       </motion.div>
 
-      <motion.div className="section_project">
+      <motion.div
+        className="section_project"
+        ref={targetRef3}
+        animate={ThirdCardInViewport ? "visible" : "hidden"}
+        variants={card}
+        initial="hidden"
+      >
         <ProjectThree variants={item}></ProjectThree>
       </motion.div>
 
-      <motion.div className="section_project">
+      <motion.div
+        className="section_project"
+        ref={targetRef4}
+        animate={FourthCardInViewport ? "visible" : "hidden"}
+        variants={card}
+        initial="hidden"
+      >
         <ProjectFour variants={item}></ProjectFour>
       </motion.div>
     </motion.section>
