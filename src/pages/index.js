985e36497f683d093a6a9b3d26804a914cddadeb
viktorrenkema <vrenkema@gmail.com>
2020-07-08 14:38:29 +0200

    Attempt for metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,24 +9,31 @@ import { Projects } from "../components/projects"
 import Framer from "../components/framer"
 import Footer from "../components/footer"
 import Groningen from "../../src/images/groningen.jpeg"
+import MetaTags from "react-meta-tags"
 
 initState()
 
 export default function IndexPage() {
   return (
     <Layout>
-      <SEO
+      <MetaTags>
+        <title>Test: Page 1</title>
+        <meta name="description" content="Test: Some description." />
+        <meta property="og:title" content="Test: MyApp" />
+        <meta property="og:image" content="../../src/images/groningen.jpeg" />
+      </MetaTags>
+      {/* <SEO
         title={"Viktor Renkema"}
         description={"An online portfolio/resume."}
         image={Groningen}
-      ></SEO>
+      ></SEO> */}
       <Intro></Intro>
       <Bio></Bio>
       <Framer></Framer>
       <Projects></Projects>
       <Scroller></Scroller>
       <Footer></Footer>
-      <SEO title="Viktor Renkema" />
+      {/* <SEO title="Viktor Renkema" /> */}
     </Layout>
   )
 }
